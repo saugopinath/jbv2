63ff5653724de49a5989df378107d412d827f3d9
shubharay21 <shubharay21@gmail.com>
2025-03-11 16:45:26 +0530

    changes auth seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -6,18 +6,21 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
-class SuperAdminUserSeeder extends Seeder
+class AdminSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        DB::table('users')->insert([
-            'name' => 'Super Admin',
-            'email' => 'superadmin@gmail.com',
+        $userId = DB::table('users')->insertGetId([
+            'mobile_no' => '7001306347',
+            'email' => 'admin@gmail.com',
             'password' => Hash::make('1234'),
+        ]);
+        DB::table('user_personals')->insert([
+            'user_id' => $userId,
+            'name' => 'Admin',
         ]);
     }
 }
