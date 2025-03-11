5a2c5649d0a9c09e373a8aa2e87068b00429165f
shubharay21 <shubharay21@gmail.com>
2025-03-11 18:51:32 +0530

    finalcommit110325

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,23 +4,23 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\UserPersonal;
 class AdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        $userId = DB::table('users')->insertGetId([
+        $user = User::create([
+            'name' => 'Admin',
             'mobile_no' => '7001306347',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('1234'),
         ]);
-        DB::table('user_personals')->insert([
-            'user_id' => $userId,
-            'name' => 'Admin',
+
+        UserPersonal::create([
+            'user_id' => $user->id,
+            'name' => $user->name,
         ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,8 @@ class DatabaseSeeder extends Seeder
             SchemeSeeder::class, 
             CodemasterSeeder::class, 
             BankSeeder::class, 
-            IfscSeeder::class, 
+            IfscSeeder::class,
+            AdminSeeder::class,
         ]);
     }
 }
