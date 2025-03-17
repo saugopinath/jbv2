9f0008c23e7fca5f7f011dcc4d233dd0f67c46bc
tithika-char <tithikachar14@gmail.com>
2025-03-18 01:07:14 +0530

    input field added for user form

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -1,20 +1,24 @@
 <?php
 
 namespace App\Filament\Resources;
-
-use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
-use App\Models\User;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
+
+use App\Models\User;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
 use Illuminate\Support\HtmlString;
+use Spatie\Permission\Models\Role;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Wizard;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ViewField;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\UserResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\UserResource\RelationManagers;
+
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
@@ -28,12 +32,53 @@ class UserResource extends Resource
                 Wizard::make([
                     Wizard\Step::make('Personal Details')
                         ->schema([
-                            // ...
+                            TextInput::make('fullname')
+                            ->label('Full Name')
+                            // ->required()
+                            ->maxLength(255),
+
+                            TextInput::make('fullname_aadhar')
+                            ->label('Full Name (as in Aadhar)')
+                            // ->required()
+                            ->maxLength(255),
+
+                        TextInput::make('display_name')
+                            ->label('Display Name')
+                            // ->required()
+                            ->maxLength(255),
+
+                        TextInput::make('email')
+                            ->email()
+                            ->unique('users', 'email')
+                            // ->required()
+                            ->maxLength(255),
+
+
+                            TextInput::make('mobile_number')
+                            ->label('Mobile Number')
+                            ->numeric()
+                            // ->required()
+                            ->minLength(10)
+                            ->maxLength(10),
+
+
                         ]),
-                    Wizard\Step::make('Role')
-                        ->schema([
-                            // ...
-                        ]),
+
+
+
+
+                            Wizard\Step::make('Role')
+                            ->schema([
+                                Select::make('role_id')
+                                    ->label('Select Role')
+                                    ->options(Role::pluck('name', 'id')->toArray())
+                                    ->searchable()
+                                    // ->required(),
+                            ]),
+
+
+
+
                     Wizard\Step::make('Office')
                         ->schema([
                             // ...
